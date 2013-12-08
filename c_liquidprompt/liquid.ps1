c9399b868621968abeaecaa9eb6b2ee260f3108e
Panayiotis Kkolos <pkkolos@gmail.com>
2013-12-08 21:03:18 +0200

    lp: synchronization between theme and ps1 files, and main file
    Add missing variables and default values.

--- c_liquidprompt/liquid.ps1
+++ c_liquidprompt/liquid.ps1
@@ -22,35 +22,25 @@
 # see the README.
 
 # add time, jobs, load and battery
-LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_JOBS}"
+LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_TEMP}${LP_JOBS}"
 LP_TITLE="${LP_PS1_PREFIX}${LP_JOBS}"
 # add user, host and permissions colon
 LP_PS1="${LP_PS1}${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
 LP_TITLE="${LP_TITLE}${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
 
-# if not root
-if [[ "$EUID" -ne "0" ]]
-then
-    # path in foreground color
-    LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
-    LP_TITLE="${LP_TITLE}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}"
-    # add VCS infos
-    LP_PS1="${LP_PS1}${LP_VCS}"
-else
-    # path in yellow
-    LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
-    LP_TITLE="${LP_TITLE}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}"
-    # do not add VCS infos unless told otherwise (LP_ENABLE_VCS_ROOT)
-    [[ "$LP_ENABLE_VCS_ROOT" = "1" ]] && LP_PS1="${LP_PS1}${LP_VCS}"
-fi
+LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
+LP_TITLE="${LP_TITLE}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}"
+
+# add VCS infos
+LP_PS1="${LP_PS1}${LP_VCS}"
+
 # add return code and prompt mark
 LP_PS1="${LP_PS1}${LP_ERR}${LP_MARK} ${LP_PS1_POSTFIX}"
 LP_TITLE="${LP_TITLE}${LP_MARK}"
 
 # "invisible" parts
 # Get the current prompt on the fly and make it a title
-#LP_TITLE=$(_lp_title $LP_PS1)
-LP_TITLE=$(_lp_title "$LP_TITLE")
+LP_TITLE="$(_lp_title "$LP_TITLE")"
 
 # Insert it in the prompt
 LP_PS1="${LP_TITLE}${LP_PS1}"
