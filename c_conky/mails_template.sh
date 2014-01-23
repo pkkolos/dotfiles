fb7653822a122ef5580111e12f217d0ceaa5d7e7
Panayiotis Kkolos <pkkolos@gmail.com>
2014-01-23 04:34:57 +0200

    conky: new conky configuration
    The Name of the Doctor

--- c_conky/mails_template.sh
+++ c_conky/mails_template.sh
@@ -4,17 +4,16 @@ if [ "$1" = "gmail" ]; then
     mail="$(conkyEmail -m IMAP --ssl -s imap.gmail.com -u <gmail> --connectiontimeout=20)" ;
 elif [ "$1" = "outlook" ]; then
     mail="$(conkyEmail -m IMAP --ssl -s imap-mail.outlook.com -u <outlook> --connectiontimeout=20)" ;
-    #mail="$(conkyEmail --ssl -s pop3.live.com -u <outlook> --connectiontimeout=20)" ;
 fi
 
 if [ "$mail" = "0" ]; then
-    mail='${color2}No new mail${color}' ;
+    mail="No new mail" ;
 elif [ "$mail" = "1" ]; then
-    mail='${color3}1 new mail${color}' ;
+    mail="1 new mail" ;
 elif [[ "$mail" =~ ^[0-9]+$ ]]; then
-    mail='${color3}'"$mail"' new mails${color}' ;
+    mail="$mail new mails" ;
 else
-    mail='${color4}Error!${color}' ;
+    mail="Error!" ;
 fi
 
 echo "$mail"
