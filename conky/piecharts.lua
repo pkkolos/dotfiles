59079aae5afaf73c8b2ecad84a232fbdb39db106
Panayiotis Kkolos <pkkolos@gmail.com>
2015-03-19 15:02:00 +0200

    conky: change fonts and tidy up the configuration file
    remove unused code

--- conky/piecharts.lua
+++
@@ -1,9 +0,0 @@
-function conky_piechart(device,fontsize)
-	percent=tonumber(conky_parse('${fs_used_perc ' .. device .. '}'))
-	if (percent) < 0 then percent="0"
-	elseif (percent)>100 then percent="100"
-	end
-	return string.format("${font Pie charts for maps:size=%i}%s${font}", 
-			fontsize, 
-			string.char(string.byte("A")+(percent/5)))
-end
--- conky/updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# execute the update of the package list
-sudo apt-get update >/dev/null
-
-# make a simulation of the upgrade and take the number of upgrade to do (O if nothing changes)
-echo "$(apt-get -s upgrade | sed -nr 's/^([0-9]+) upgraded.*$/\1/p')"
-
-exit 0
