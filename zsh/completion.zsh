151a57e556d96b29e9160242064381f2f785c5c1
Panagiotis Kkolos <pkkolos@gmail.com>
2016-12-08 04:56:32 +0200

    zsh: matcher not always case-insensitive

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,7 +1,7 @@
 zstyle ':completion:*'               completer _expand _complete _list _match _approximate
 zstyle ':completion:*'               matcher-list 'm:{[:lower:]}={[:upper:]}' \
-                                                  'm:{[:lower:]}={[:upper:]} r:|[._-]=* r:|=*' \
-                                                  'm:{[:lower:]}={[:upper:]} l:|=*'
+                                                  'r:|[._-]=* r:|=*' \
+                                                  'l:|=*'
 zstyle ':completion:*:expand:*'      group-order all-expansions
 zstyle ':completion:*:match:*'       original only
 zstyle ':completion:*:approximate:*' max-errors 2 numeric
