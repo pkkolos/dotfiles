a76a320c9dd2e41f87697f371d661d62ad589b8b
Panagiotis Kkolos <pkkolos@gmail.com>
2022-09-19 18:49:16 +0300

    nautilus: update open-terminal script

--- open-terminal.py
+++ open-terminal.py
@@ -1,7 +1,4 @@
 import os
-import gi
-
-gi.require_version('Nautilus', '3.0')
 
 from gi.repository import Nautilus, GObject
 
@@ -9,21 +6,21 @@ class OpenTerminalExtension(GObject.GObject, Nautilus.MenuProvider):
     def _open_terminal(self, menu, directory):
         os.system('alacritty --working-directory "%s" &' % directory.get_location().get_path())
 
-    def get_file_items(self, window, files):
+    def get_file_items(self, files):
         if len(files) != 1:
             return
 
-        file_ = files[0]
-        if not file_.is_directory() or file_.get_uri_scheme() != 'file':
+        file = files[0]
+        if not file.is_directory() or file.get_uri_scheme() != 'file':
             return
 
         item = Nautilus.MenuItem(name='OpenTerminalExtension::open_in_dir',
                                  label='Open In Alacritty',
-                                 tip='Open Terminal In %s' % file_.get_name())
-        item.connect('activate', self._open_terminal, file_)
+                                 tip='Open Terminal In %s' % file.get_name())
+        item.connect('activate', self._open_terminal, file)
         return [item]
 
-    def get_background_items(self, window, directory):
+    def get_background_items(self, directory):
         item = Nautilus.MenuItem(name='OpenTerminalExtension::open_here',
                                  label='Open Alacritty Here',
                                  tip='Open Terminal In This Directory')
